cccdb7ec59ba397c00c424728e5a54af2a350adc
Andrea Fomera <afomera@hey.com>
2025-02-11 19:57:55 -0700

    try symlink for the install script

--- scripts/install_macos.sh
+++ scripts/install_macos.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Create the destination directory if it doesn't exist
 mkdir -p /usr/local/bin
 
-# Extract the binary from the app bundle using ditto to preserve code signing
-ditto "$DIR/Spin.app/Contents/MacOS/spin" /usr/local/bin/spin
+# Create a symlink to the binary inside the app bundle
+ln -sf "$DIR/Spin.app/Contents/MacOS/spin" /usr/local/bin/spin
 
 echo "Spin CLI installed successfully! Run 'spin' to get started."
